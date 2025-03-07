a570f583a9c1029a2ec653db1d004ba01f628458
dinodin <dinodin@dinodins-MacBook-Pro.local>
2025-03-07 01:26:52 +0000

    create router,controller and model function for GET api/habits/:user_id endpoint

--- controllers/habits-controller.js
+++ controllers/habits-controller.js
@@ -1,4 +1,4 @@
-import { createHabit } from "../models/habits-model.js";
+import { createHabit, fetchUserHabits } from "../models/habits-model.js";
 
 export function addHabit(req, res, next) {
   const reqBody = req.body;
@@ -7,6 +7,18 @@ export function addHabit(req, res, next) {
   createHabit(reqBody, user_id)
     .then((addedHabit) => {
       res.status(201).send({ addedHabit: addedHabit });
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+export function getUserHabits(req, res, next) {
+  const { user_id } = req.params;
+
+  fetchUserHabits(user_id)
+    .then((userHabits) => {
+      res.status(200).send({ userHabits: userHabits });
     })
     .catch((err) => {
       next(err);
